600d27a5e05396b131b088efb8ebe4dba47d470a
Adrian Cruz <drincruz@gmail.com>
2019-06-16 18:50:52 -0400

    Remove Specifics for Twitter Screen Name
    More general re-usability

--- getweet.go
+++ getweet.go
@@ -14,9 +14,10 @@ func getTweet() string {
 	tokenSecret := os.Getenv("TWITTER_TOKEN_SECRET")
 	consumerKey := os.Getenv("TWITTER_CONSUMER_KEY")
 	consumerSecret := os.Getenv("TWITTER_CONSUMER_SECRET")
+	twitterScreenName := os.Getenv("TWITTER_SCREEN_NAME")
 	api := anaconda.NewTwitterApiWithCredentials(token, tokenSecret, consumerKey, consumerSecret)
 	twitter := url.Values{}
-	twitter.Set("screen_name", "drincruz")
+	twitter.Set("screen_name", twitterScreenName)
 	twitter.Add("count", "1")
 	tweets, _ := api.GetUserTimeline(twitter)
 	return tweets[0].Text
