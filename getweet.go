42397d8f47ffa79f0f0f8993fb7d728af422bb6a
Adrian Cruz <drincruz@gmail.com>
2019-06-16 18:29:32 -0400

    Initial Commit
     - Things are working locally

---
+++ getweet.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/ChimeraCoder/anaconda"
+)
+
+// Gets the latest Tweet and returns its tweet.Text
+func getTweet() string {
+	token := os.Getenv("TWITTER_TOKEN")
+	tokenSecret := os.Getenv("TWITTER_TOKEN_SECRET")
+	consumerKey := os.Getenv("TWITTER_CONSUMER_KEY")
+	consumerSecret := os.Getenv("TWITTER_CONSUMER_SECRET")
+	api := anaconda.NewTwitterApiWithCredentials(token, tokenSecret, consumerKey, consumerSecret)
+	twitter := url.Values{}
+	twitter.Set("screen_name", "drincruz")
+	twitter.Add("count", "1")
+	tweets, _ := api.GetUserTimeline(twitter)
+	return tweets[0].Text
+}
+
+// Main
+func main() {
+	fmt.Println(getTweet())
+	return
+}
